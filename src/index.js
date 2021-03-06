c76356c2125707618dca08e0fc1e4a9a43ac2c23
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-03-05 22:48:36 -0300

    chore: fist endpoint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const router = require('./routes/index.routing');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(router);
 
-
-app.listen(port,()=>{
- console.log('server is running on port: ', port);
+app.listen(port, () => {
+    console.log('server is running on port: ', port);
 });
