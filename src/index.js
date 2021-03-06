15946c3a6dc971b7095f2d3fbfa96442a8f0b78b
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-03-05 22:37:00 -0300

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+
+app.listen(port,()=>{
+ console.log('server is running on port: ', port);
+});
