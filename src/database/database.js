55bba42c46a3533391dc7ba30a824d14b1781bd5
Jessica Suelen Claudino de Andrade <jessica.dev.andrade@gmail.com>
2021-04-06 19:39:36 -0300

    modificações de conexão de database

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
-
-
+require('dotenv').config()
 const data_url = process.env.DB_URL;
-
-const replace_url = data_url.replace('<user>', process.env.DB_USER).replace('<password>', process.env.DB_PASSWORD);
-
-
-mongoose.connect(replace_url, { useNewUrlParser: true });
+const replace_url = data_url.replace('<user>', process.env.DB_USER).replace('<password>', process.env.DB_PASSWORD).replace('<database>', process.env.DB_NAME).replace('<database>', process.env.DB_NAME);
+mongoose.connect(
+    replace_url.toString(),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  
 const connection = mongoose.connection;
 
 connection.on('open',()=>console.log('Mongo: Connection OPEN!'));
