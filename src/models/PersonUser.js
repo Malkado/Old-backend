4b6be75681213cf50bf339fc832370f35e3e3988
Jessica Suelen Claudino de Andrade <jessica.dev.andrade@gmail.com>
2021-04-06 21:50:41 -0300

    crud de usuari sem jwt

---
+++ src/models/PersonUser.js
@@ -0,0 +1,36 @@
+const mongoose = require('../database/database');
+const bcrypt = require('bcryptjs')
+
+const PersonUserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        require: true,
+        lowercase: true,
+    },
+    password:{
+        type: String,
+        require: true,
+        select: false,
+    },
+    createAt:{
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// Irá encriptar o password
+PersonUserSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+
+const PersonUser = mongoose.model('PersonUser', PersonUserSchema);
+
+module.exports = PersonUser;
