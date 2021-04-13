c16e694267545c3d16280149cce44d9c0a266cfe
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-04-13 00:35:57 -0300

    chore: config nodemailer transport

--- src/controller/emailController.js
+++ src/controller/emailController.js
@@ -4,12 +4,12 @@ const response = require('../helper/response-helper');
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT, // true for 465, false for other ports
+    port: parseInt(process.env.EMAIL_PORT), 
+    secure: false,// true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: { rejectUnauthorized: false }
+    }
 });
 module.exports = {
     async sendConfirmEmail(req, res) {
@@ -19,12 +19,14 @@ module.exports = {
             const { email } = resultFindUserByEmail;
             const mailOptions = {
                 from: process.env.EMAIL_USER,
+                replyTo: process.env.EMAIL_USER,
                 to: email,
                 subject: 'E-mail enviado usando Node!',
                 text: 'Bem fácil, não? ;)'
             };
             transporter.sendMail(mailOptions, function (error, info) {
                 if (error) {
+                    transporter.close();
                     console.log(error);
                 } else {
                     console.log('Email enviado: ' + info.response);
