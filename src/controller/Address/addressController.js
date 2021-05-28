0538f3334377174f585015080a5e8d52b876ac37
JessicaSAndrade <jessica.1999.2009@gmail.com>
2021-05-28 12:20:51 -0300

    correção de endereço

--- src/controller/Address/addressController.js
+++ src/controller/Address/addressController.js
@@ -1,18 +1,72 @@
-
 const Address = require('../../models/Address/Address');
 const AuthUser = require('../../models/AuthUser');
 const response = require('../../helper/response-helper');
 
 module.exports = {
 
+    // async registerAddress(req, res) {
+    //     try {
+    //         const address = await Address.create(req.body);
+    //         address.sequence_id = undefined;
+
+    //         return res.status(201).send({ message: 'Endereço registrado com sucesso!' })
+    //     } catch (err) {
+    //         return res.status(400).send({ error: 'Erro no registro do endereço!' })
+    //     }
+    // },
+
     async registerAddress(req, res) {
         try {
-            const address = await Address.create(req.body);
-            address.sequence_id = undefined;
+            const {
+                street,
+                city,
+                state,
+                zipcode,
+                neighborhood,
+                uf,
+                country,
+                type_user,
+                id_user
+            } = req.body;
 
-            return res.status(201).send({ message: 'Endereço registrado com sucesso!' })
-        } catch (err) {
-            return res.status(400).send({ error: 'Erro no registro do endereço!' })
+            if (!street, !city, !state, !zipcode, !neighborhood, !uf, !country, !type_user, !id_user) {
+                const status = 400;
+                const message = 'Parâmetros inválidos.';
+                return res.json(response.responseMensage([], message, status));
+            }
+            // const checkUser = await AuthUser.findById(userId);
+            // if (!checkUser) {
+            //     const status = 404;
+            //     const message = 'Usuário não encontrado.';
+            //     return res.json(response.responseMensage([], message, status));
+            // }
+
+
+            const body = {
+                "type_user": type_user,
+                "id_user": id_user,
+                "street": street,
+                "city": city,
+                "state": state,
+                "zipcode": zipcode,
+                "neighborhood": neighborhood,
+                "uf": uf,
+                "country": country
+            };
+            const createAddress = await Address.create(body);
+            // const getAddress = await Address.findOneAndUpdate({ sequence_id: address_Id }, bodyupdate);
+            if (!createAddress) {
+                const status = 500;
+                const message = 'Erro ao tentar atualizar.';
+                return res.json(response.responseMensage([], message, status));
+            }
+            const status = 200;
+            const message = 'Função executada com sucesso.';
+            return res.json(response.responseMensage([], message, status));
+        } catch (e) {
+            const status = 500;
+            const message = 'Erro interno da função.';
+            return res.json(response.responseMensage([], message, status));
         }
     },
 
@@ -21,7 +75,7 @@ module.exports = {
             var arr = [];
             const user_id = req.params.id;
             const type_user = req.params.type;
-            await Address.find({}, function (err, docs) {
+            await Address.find({}, function(err, docs) {
                 docs.forEach(element => {
                     if (element['id_user'] == user_id && element['type_user'] == type_user) {
                         arr.push(element);
@@ -39,7 +93,7 @@ module.exports = {
             let addressRemove;
             const id_user = req.params.id;
             const type_user = req.params.type;
-            await Address.find({ id_user: id_user, type_user: type_user }, function (err, docs) {
+            await Address.find({ id_user: id_user, type_user: type_user }, function(err, docs) {
                 addressRemove = docs;
             });
             await Address.findOneAndDelete(addressRemove);
@@ -55,13 +109,20 @@ module.exports = {
 
     async updateUserAddress(req, res) {
         try {
-            const { address_Id, street, city, state,
-                zipcode, neighborhood, uf, country } = req.body;
+            const {
+                address_Id,
+                street,
+                city,
+                state,
+                zipcode,
+                neighborhood,
+                uf,
+                country
+            } = req.body;
             const { userId } = req;
 
 
-            if (!address_Id, !street, !city, !state,
-                !zipcode, !neighborhood, !uf, !country) {
+            if (!address_Id, !street, !city, !state, !zipcode, !neighborhood, !uf, !country) {
                 const status = 400;
                 const message = 'Parâmetros inválidos.';
                 return res.json(response.responseMensage([], message, status));
